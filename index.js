c2dc13b0f2aa0530ba49fec3d1d5da8d757aa9ee
max10020 <legendduty10020@gmail.com>
2022-04-06 11:35:02 +0200

    new task completed

--- index.js
+++ index.js
@@ -2,16 +2,36 @@ const express = require("express"),
     path = require("path"),
     bodyParser = require("body-parser")
 
+
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+app.get(`/`, (req,res)=>{
+    const key = req.query
+    res.send(key)
+})
+
+app.post(`/`, (req,res)=>{
+    res.send(req.body)
+})
+
+app.put(`/:id`, (req,res)=>{
+    res.send(req.params.id)
+})
+
+app.delete(`/:id/`, (req,res)=>{
+    const id = req.params.id;
+    const query = req.query
+    const query_params = {id, query}
+    res.send(query_params)
+})
 
 let port = process.env.PORT || 8080;
 app.listen(port, ()=>{
     console.log(`Listening on ` + port);
-})
+});
 
-app.get(`/hello`, (req, res)=>{
-    res.send(`Hello!`)
-})
+
